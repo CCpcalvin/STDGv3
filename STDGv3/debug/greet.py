2963cc0566813e5806a296c0986b6a7220b584ee
CCpcalvin <chunpangchan2001@gmail.com>
2025-01-03 21:59:00 +0800

    Init Project

---
+++ STDGv3/debug/greet.py
@@ -0,0 +1,9 @@
+
+def greet():
+    print("Hello World")
+
+def greet2():
+    print("Hello World2")
+
+def greet3():
+    print("Hello World3")
---
+++ tests/greetTest.py
@@ -0,0 +1,11 @@
+import STDGv3
+
+
+def main():
+    STDGv3.greet()
+    STDGv3.greet2()
+    STDGv3.greet3()
+
+
+if __name__ == "__main__":
+    main()
